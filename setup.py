6bebb390546f21471a9a581ddce9207860ca31ea
dbeley <6568955+dbeley@users.noreply.github.com>
2021-12-28 00:49:00 +0100

    Disable 3.10 support in CI.

--- setup.py
+++ setup.py
@@ -20,5 +20,5 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "Operating System :: POSIX :: Linux",
     ],
-    install_requires=["yt-dlp", "pandas", "numpy", "openpyxl"],
+    install_requires=["yt-dlp", "pandas", "openpyxl"],
 )
